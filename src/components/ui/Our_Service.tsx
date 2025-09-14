f281e42abc6805b4e5fa79b87e8d3ec82612485c
Amonpuwit Thitaphattharaphong <amonpuwitthi@gosoft.co.th>
2025-09-14 09:32:49 +0700

    14/9/2025 แก้ไขคำ

--- src/components/ui/Our_Service.tsx
+++ src/components/ui/Our_Service.tsx
@@ -4,7 +4,7 @@ const Our_Serviec = () => {
   return (
     <div className="about-section text-black">
       <h2>ข้อมูล</h2>
-      <h2 style={{color:'black',fontSize:'20px'}}>นี้คือข้อมูลเพื่อใช้ytestในการอับเดทของVercel</h2>
+      <h2 style={{color:'black',fontSize:'20px'}}>นี้คือข้อมูลเพื่อใช้testในการอับเดทของVercel</h2>
     </div>
   );
 };
